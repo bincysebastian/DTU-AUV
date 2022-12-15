af126739451b112f5aa09d209d77f664cceadecd
Bincy Sebastian <115625696+bincysebastian@users.noreply.github.com>
2022-12-15 16:41:11 +0530

    Create main.cpp

---
+++ colour detection/main.cpp
@@ -0,0 +1,35 @@
+#include<opencv2/highgui.hpp>
+#include<opencv2/imgcodecs.hpp>
+#include<opencv2/imgproc.hpp>
+#include<iostream>
+
+using namespace std;
+using namespace cv;
+
+int main()
+{ Mat imgHSV,mask;
+int hmin=70,smin=160,vmin=115;
+int hmax=86,smax=210,vmax=134;
+ string path="sample.jpeg";
+ Mat img = imread(path);
+cvtColor(img,imgHSV,COLOR_BGR2HSV);
+namedWindow("Trackbars",(250,640));
+createTrackbar("Hue Min","Trackbars",&hmin,179);
+createTrackbar("Hue Max","Trackbars",&hmax,179);
+createTrackbar("Sat Min","Trackbars",&smin,255);
+createTrackbar("Sat Max","Trackbars",&smax,255);
+createTrackbar("Val Min","Trackbars",&vmin,255);
+createTrackbar("Val Max","Trackbars",&vmax,255);
+
+while(true)
+{
+Scalar lower(hmin,smin,vmin);
+Scalar upper(hmax,smax,vmax);
+ inRange(imgHSV,lower,upper,mask);
+ imshow("Image",img);
+ imshow("Image hsv",imgHSV);
+ imshow("Image mask",mask);
+ waitKey(1);
+}
+ return 0;
+}
